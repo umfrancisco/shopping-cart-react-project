9c7754f7681763f138410712ac4d58244bd2d8d0
umfrancisco <francis.guitler@gmail.com>
2026-05-28 15:31:50 -0300

    Add header component and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,30 @@
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
 const Header = () => (
-	<header>
-		header
-	</header>
+	<HeaderBar>
+		<div>
+			<img src={logo} alt="logo" />
+			<nav>
+				<Links>
+					<LinkItem>
+						<a href="#">Categorias</a>
+					</LinkItem>
+					<LinkItem>
+						<a href="#">Novidades</a>
+					</LinkItem>
+					<LinkItem>
+						<a href="#">Novidades</a>
+					</LinkItem>
+				</Links>
+			</nav>
+		</div>
+		<LinkCart href="#">
+			0 - produto(s)
+			<img src={carrinho} alt="carrinho icone" />
+		</LinkCart>
+	</HeaderBar>
 );
 
 export default Header
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, styled } from 'styled-components';
 
-const cores = {
+export const cores = {
 	branca: '#EEE',
 	preta: '#111',
 	cinza: '#333',
@@ -12,10 +12,18 @@ export const GlobalCss = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+		list-style: none;
 	}
 	
 	body {
 		background-color: ${cores.preta};
 		color: ${cores.branca};
+		padding-top: 40px;
 	}
+`;
+
+export const Container = styled.div`
+	max-width: 1024px;
+	width: 100%;
+	margin: 0 auto;
 `;
